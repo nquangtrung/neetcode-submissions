f7cef5afd0753e9c383143735c1979dc9b474156
Nguyễn Quang Trung <nquangtrung@gmail.com>
2026-09-08 16:11:58 +0200

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.go
@@ -0,0 +1,22 @@
+func twoSum(numbers []int, target int) []int {
+	for ptr1, ptr2 := 0, len(numbers) - 1; ptr1 < ptr2; {
+		num1 := numbers[ptr1]
+		num2 := numbers[ptr2]
+		sum := num1 + num2
+		if sum == target {
+			return []int{ptr1 + 1, ptr2 + 1}
+		}
+
+		if sum > target {
+			ptr2 -= 1
+			continue
+		}
+
+		if sum < target {
+			ptr1 += 1
+			continue
+		}
+	}
+
+	return []int{}
+}
